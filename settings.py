bb7a6dab0046ef2d8a974f6117b1badd5e27735b
Neskie Manuel <neskiem@gmail.com>
2010-08-15 17:25:32 -0700

    more portable settings

--- settings.py
+++ settings.py
@@ -1,55 +1,22 @@
 # Django settings for radio project.
 import os
-MEDIA_ROOT = '/srv/apache/radiostatic/media/'
+from local_settings import *
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media/')
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-from local_settings import *
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = 'America/Vancouver'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
-
 SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
 USE_L10N = True
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = 'http://secwepemcradio.ath.cx/media/'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/media/admin/'
-
-
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -60,12 +27,8 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'radio.urls'
-
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    '/srv/apache/django-projects/radio/templates/'
+	os.path.join(PROJECT_ROOT, 'templates/')
 )
 
 INSTALLED_APPS = (
@@ -74,7 +37,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'django.contrib.admindocs',
     'django.contrib.flatpages',
@@ -83,7 +45,6 @@ INSTALLED_APPS = (
     'django.contrib.comments',
     'django.contrib.sitemaps',
     'weblog',
-#    'django_evolution',
     'tagging',
     'django_extensions',
     'show',
